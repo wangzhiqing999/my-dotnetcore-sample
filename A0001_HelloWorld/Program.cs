9cb991c296248b5cac74a1f0da6db894cfa29742
wangzhiqing999 <wangzhiqing999@hotmail.com>
2017-06-01 18:53:07 +0800

    hello world only

---
+++ A0001_HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace A0001_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for(int i=0; i < 10; i ++) {
+                Console.WriteLine("Hello {0}", i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# my-dotnetcore-sample
+
+
+### A0001_HelloWorld
+最简单的 HelloWorld 程序.
+
+
+### A0002_HelloWorldMvc
+最简单的 HelloWorld  asp.net core 程序.
