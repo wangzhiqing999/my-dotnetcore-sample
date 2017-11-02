04961fb91bb894318bdaddcf3f0931156bbcd50e
wangzhiqing999 <wangzhiqing999@hotmail.com>
2017-11-02 19:30:45 +0800

    Add EF_Sqlite

---
+++ A0006_EF_Sqlite/A0006_EF_Sqlite/DataAccess/TestContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+using Microsoft.EntityFrameworkCore;
+
+
+using A0006_EF_Sqlite.Model;
+
+
+namespace A0006_EF_Sqlite.DataAccess
+{
+
+    public class TestContext : DbContext
+    {
+
+        public TestContext() : base()
+        {
+        }
+
+        public TestContext(DbContextOptions<TestContext> options) : base(options)
+        {
+        }
+
+
+        /// <summary>
+        /// 测试数据.
+        /// </summary>
+        public DbSet<TestData> TestDatas { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            {
+                if (optionsBuilder.IsConfigured == false)
+                {
+                    optionsBuilder.UseSqlite(@"Data Source=test.db");
+                }
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+    }
+}
---
+++ A0006_EF_Sqlite/A0006_EF_Sqlite/Migrations/20171102111133_FirstMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace A0006_EF_Sqlite.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "test_data",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    address = table.Column<string>(type: "TEXT", maxLength: 64, nullable: true),
+                    name = table.Column<string>(type: "TEXT", maxLength: 16, nullable: true),
+                    phone = table.Column<string>(type: "TEXT", maxLength: 16, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_test_data", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "test_data");
+        }
+    }
+}
