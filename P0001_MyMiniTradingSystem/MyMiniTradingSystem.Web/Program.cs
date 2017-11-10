cbeba356c32f3dceb02a8d317ae58f4aacbcc971
wangzhiqing999 <wangzhiqing999@hotmail.com>
2017-11-10 16:49:58 +0800

    test run on CentOS7

--- P0001_MyMiniTradingSystem/MyMiniTradingSystem.Web/Program.cs
+++ P0001_MyMiniTradingSystem/MyMiniTradingSystem.Web/Program.cs
@@ -17,9 +17,21 @@ namespace MyMiniTradingSystem.Web
             BuildWebHost(args).Run();
         }
 
+
+
+        // 注意：这里额外增加的 .UseUrls("http://*:5000") 。 
+        // 是为了在 Linux 下面，能够正常的测试运行。
+        // 而不是去报 
+        // warn: Microsoft.AspNetCore.Server.Kestrel[0]
+        // Unable to bind to http://localhost:5000 on the IPv6 loopback interface: 'Error -99 EADDRNOTAVAIL address not available'.
+        // 这样的错误.
         public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            WebHost.CreateDefaultBuilder(args)                
+                .UseUrls("http://*:5000")
                 .UseStartup<Startup>()
                 .Build();
+
+
+
     }
 }
