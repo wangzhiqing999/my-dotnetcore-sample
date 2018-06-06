3b35a97b8ae49c41d6bc173c6597b7bd82c86393
wangzhiqing999 <wangzhiqing999@hotmail.com>
2018-06-06 18:05:47 +0800

    vue 翻页

---
+++ W1100_Page/W1100_Page.Web/Controllers/FinanceVueController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace W1100_Page.Web.Controllers
+{
+    public class FinanceVueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// 更多 的方式翻页.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult More()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// 滚动条 的方式翻页.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Scroll()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// 标准翻页 的方式.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Page()
+        {
+            return View();
+        }
+
+
+
+    }
+}
