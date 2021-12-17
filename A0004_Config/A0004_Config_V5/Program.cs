3cc72eb1477ee825cd6c7b28873eb66e1e5ab417
wangzhiqing999 <wangzhiqing999@hotmail.com>
2021-12-17 14:56:28 +0800

    Update A0004_Config
    Add read  propertie, yaml, Consul, Etcd  Sample

---
+++ A0004_Config/A0004_Config_V5/Program.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Ini;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration.Xml;
+using Yt.Extensions.Configuration.Properties;
+using Yt.Extensions.Configuration.Yaml;
+using Yt.Extensions.Configuration.Consul;
+using Yt.Extensions.Configuration.Etcd;
+
+namespace A0004_Config_V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestReadIni();
+            TestReadIni2();
+
+
+            TestReadJson();
+            TestReadJson2();
+
+
+            TestReadXml();
+            TestReadXml2();
+
+
+            TestReadProperties2();
+
+
+            TestReadYaml2();
+
+            TestReadConsul2();
+
+            TestReadEtcd2();
+
+            Console.ReadKey();
+        }
+
+
+
+
+        #region Ini
+
+        /// <summary>
+        /// 测试读取 ini 文件.
+        /// </summary>
+        static void TestReadIni()
+        {
+            Console.WriteLine("##### Test read config.ini file #####");
+
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            IniConfigurationSource source = new IniConfigurationSource();
+            source.Build(builder);
+            source.Path = "config.ini";
+            IniConfigurationProvider provider = new IniConfigurationProvider(source);
+            provider.Load();
+
+
+            string segoneCon = null;
+            provider.TryGet("SegOne:Con", out segoneCon);
+            Console.WriteLine($"SegOne-Con={segoneCon}");
+
+            string segtwoCon = null;
+            provider.TryGet("SegTwo:Con", out segtwoCon);
+            Console.WriteLine($"SegTwo-Con={segtwoCon}");
+
+            string segtwoExtPort = null;
+            provider.TryGet("SegTwo:Ext:Port", out segtwoExtPort);
+            Console.WriteLine($"SegTwo-Ext:Port={segtwoExtPort}");
+
+            string segthreeCon = null;
+            provider.TryGet("Seg:Three:Con", out segthreeCon);
+            Console.WriteLine($"Seg:Three-Con={segthreeCon}");
+
+            Console.WriteLine();
+        }
+
+        static void TestReadIni2()
+        {
+            Console.WriteLine("##### Test read config.ini file All #####");
+
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddIniFile("config.ini");
+
+            IConfiguration config = builder.Build();
+            foreach (var item in config.AsEnumerable())
+            {
+                Console.WriteLine($"Key:{item.Key}----Value:{item.Value}");
+            }
+            Console.WriteLine();
+        }
+
+
+        #endregion
+
+
+
+
+
+
+        #region json
+
+
+
+        /// <summary>
+        /// 测试读取 json 文件.
+        /// </summary>
+        static void TestReadJson()
+        {
+            Console.WriteLine("##### Test read config.json file #####");
+
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            JsonConfigurationSource source = new JsonConfigurationSource();
+            source.Build(builder);
+            source.Path = "config.json";
+            JsonConfigurationProvider provider = new JsonConfigurationProvider(source);
+            provider.Load();
+
+            string url = null;
+            provider.TryGet("url", out url);
+            Console.WriteLine($"url={url}");
+
+            string one = null;
+            provider.TryGet("port:one", out one);
+            Console.WriteLine($"port-one={one}");
+
+
+            string two = null;
+            provider.TryGet("port:two", out two);
+            Console.WriteLine($"port0two={two}");
+
+
+            Console.WriteLine();
+        }
+
+
+        static void TestReadJson2()
+        {
+            Console.WriteLine("##### Test read config.json file All #####");
+
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json");
+
+            IConfiguration config = builder.Build();
+            foreach (var item in config.AsEnumerable())
+            {
+                Console.WriteLine($"Key:{item.Key}----Value:{item.Value}");
+            }
+            Console.WriteLine();
+        }
+
+
+        #endregion
+
+
+
+
+
+
+        #region XML
+
+        /// <summary>
+        /// 测试读取 XML 文件.
+        /// </summary>
+        static void TestReadXml()
+        {
+            Console.WriteLine("##### Test read config.xml file #####");
+
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            XmlConfigurationSource source = new XmlConfigurationSource();
+            source.Build(builder);
+            source.Path = "config.xml";
+            XmlConfigurationProvider provider = new XmlConfigurationProvider(source);
+            provider.Load();
+
+            string con = null;
+            provider.TryGet("data:con", out con);
+            Console.WriteLine($"con={con}");
+
+            string name = null;
+            provider.TryGet("inventory:value", out name);
+            Console.WriteLine($"value={name}");
+
+
+            Console.WriteLine();
+
+        }
+
+
+
+
+        static void TestReadXml2()
+        {
+            Console.WriteLine("##### Test read config.xml file All #####");
+
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddXmlFile("config.xml");
+
+            IConfiguration config = builder.Build();
+            foreach (var item in config.AsEnumerable())
+            {
+                Console.WriteLine($"Key:{item.Key}----Value:{item.Value}");
+            }
+            Console.WriteLine();
+        }
+
+        #endregion
+
+
+
+        #region Properties
+
+
+        static void TestReadProperties2()
+        {
+            Console.WriteLine("##### Test read config.properties file All #####");
+
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddPropertiesFile("config.properties");
+
+            IConfiguration config = builder.Build();
+            foreach (var item in config.AsEnumerable())
+            {
+                Console.WriteLine($"Key:{item.Key}----Value:{item.Value}");
+            }
+            Console.WriteLine();
+        }
+
+
+
+        #endregion
+
+
+
+
+
+        #region Yaml
+
+
+        static void TestReadYaml2()
+        {
+            Console.WriteLine("##### Test read config.yaml file All #####");
+
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddYamlFile("config.yaml");
+
+            IConfiguration config = builder.Build();
+            foreach (var item in config.AsEnumerable())
+            {
+                Console.WriteLine($"Key:{item.Key}----Value:{item.Value}");
+            }
+            Console.WriteLine();
+        }
+
+
+
+        #endregion
+
+
+
+
+
+
+
+        #region consul
+
+
+        static void TestReadConsul2()
+        {
+            string serverUrl = @"http://192.168.1.153:8500/";
+
+            Console.WriteLine($"##### Test read consul {serverUrl}  key/value All #####");
+
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddConsul(serverUrl, "TestCode", true, 10 * 1000);
+
+
+            IConfiguration config = builder.Build();
+            foreach (var item in config.AsEnumerable())
+            {
+                Console.WriteLine($"Key:{item.Key}----Value:{item.Value}");
+            }
+            Console.WriteLine();
+        }
+
+
+        #endregion
+
+
+
+
+
+        #region Etcd
+
+
+        static void TestReadEtcd2()
+        {
+            string serverUrl = @"http://192.168.1.153:2379/";
+
+            Console.WriteLine($"##### Test read Etcd {serverUrl}  key/value All #####");
+
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddEtcd(serverUrl, "e3w_test/ABC", true);
+
+
+            IConfiguration config = builder.Build();
+            foreach (var item in config.AsEnumerable())
+            {
+                Console.WriteLine($"Key:{item.Key}----Value:{item.Value}");
+            }
+            Console.WriteLine();
+        }
+
+
+        #endregion
+
+
+    }
+}
--- A0004_Config/readme.md
+++ A0004_Config/readme.md
@@ -1,4 +1,4 @@
-.NET Core 配置文件操作.
+# .NET Core 配置文件操作.
 
 
 本项目在 .Net Core 2.0 版本下运行。
@@ -10,3 +10,116 @@ Microsoft.Extensions.Configuration.Json
 Microsoft.Extensions.Configuration.Xml
 
 用于解析  ini , json,  xml  文件。
+
+
+
+
+
+
+
+
+
+## A0004_Config_V5
+使用 .NET 5.0 版本.
+
+
+Nuget 获取
+Microsoft.Extensions.Configuration.Ini
+Microsoft.Extensions.Configuration.Json
+Microsoft.Extensions.Configuration.Xml
+
+用于解析  ini , json,  xml  文件。
+
+
+
+Nuget 获取
+Yt.Extensions.Configuration.Properties
+Yt.Extensions.Configuration.Yaml
+
+用于解析  propertie , yaml 文件。
+
+
+Nuget 获取
+Yt.Extensions.Configuration.Consul
+Yt.Extensions.Configuration.Etcd
+
+用于解析  Consul , Etcd 服务器上的配置数据。
+
+
+
+
+
+
+### Consul 安装与测试
+
+测试服务器 192.168.1.153
+
+安装
+sudo docker run --name consul -d -p 8500:8500 consul
+
+查看管理页
+http://192.168.1.153:8500/
+
+在 【Key/Value】 的地方，添加用于测试的 配置信息.
+添加一个 Key = TestCode
+Value =  一段 Json 字符串
+【Save】
+
+
+
+
+
+
+
+### Etcd 安装与测试
+
+测试服务器 192.168.1.153
+
+安装
+export NODE1=192.168.1.153
+
+sudo docker run --name etcd \
+    -p 2379:2379 \
+    -p 2380:2380 \
+    quay.io/coreos/etcd:latest \
+    /usr/local/bin/etcd \
+    --data-dir=/etcd-data --name node1 \
+    --initial-advertise-peer-urls http://${NODE1}:2380 --listen-peer-urls http://0.0.0.0:2380 \
+    --advertise-client-urls http://${NODE1}:2379 --listen-client-urls http://0.0.0.0:2379 \
+    --initial-cluster node1=http://${NODE1}:2380
+
+
+本机 /home/wang/e3w/conf/ 目录下，准备好一个配置文件
+wang@wang001:~/e3w/conf$ cat config.default.ini
+[app]
+port=8080
+auth=false
+
+[etcd]
+root_key=e3w_test
+dir_value=
+addr=192.168.1.153:2379
+username=
+password=
+cert_file=
+key_file=
+ca_file=
+
+
+运行
+sudo docker run  --name e3w \
+  -d \
+  -p 8080:8080 \
+  -v /home/wang/e3w/conf/config.default.ini:/app/conf/config.default.ini \
+  soyking/e3w:latest
+
+
+浏览器访问
+http://192.168.1.153:8080/
+添加一个 
+Key = ABC
+Value =  一段 Json 字符串
+【UPDATE】
+
+
+
