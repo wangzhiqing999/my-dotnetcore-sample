3bec41797b113afd87fe9e154767cb3b583546b4
wangzhiqing999 <wangzhiqing999@hotmail.com>
2022-12-15 20:25:06 +0800

    Update A0050_BlazorApp
    添加 父子组件之间的通信例子

---
+++ A0050_BlazorApp/A0050_BlazorApp/Components/CascadingModel.cs
@@ -0,0 +1,29 @@
+﻿namespace A0050_BlazorApp.Components
+{
+    public class CascadingModel<T>
+    {
+        public CascadingModel()
+        {
+
+        }
+
+        public CascadingModel(T defaultValue)
+        {
+            _value = defaultValue;
+        }
+        public Action StateHasChanged;
+
+        private T _value;
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                StateHasChanged?.Invoke();
+            }
+        }
+
+
+    }
+}
