2779ce469ed3ee7ac1b3cc55ab0acda9eb5f9d52
wangzhiqing999 <wangzhiqing999@hotmail.com>
2024-11-12 13:20:34 +0800

    Update Table Sample
    追加 AntDesign 使用 Table组件 的例子

---
+++ W4101_AntDesignApp/W4113_AntDesignProServer/Models/Test/MyAction.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace W4113_AntDesignProServer.Models.Test
+{
+
+    /// <summary>
+    /// 模块动作.
+    /// </summary>
+    [DisplayName("模块动作")]
+    public class MyAction
+    {
+
+
+        /// <summary>
+        /// 动作代码.
+        /// </summary>
+        [Display(Name = "动作代码")]
+        [Required]
+        public string ActionCode { set; get; }
+
+
+        /// <summary>
+        /// 动作名称.
+        /// </summary>
+        [Display(Name = "动作名称")]
+        [Required]
+        public string ActionName { set; get; }
+
+
+
+
+        /// <summary>
+        /// 模块编号.
+        /// </summary>
+        [Display(Name = "模块编号")]
+        [Required]
+        public string ModuleCode { set; get; }
+
+
+
+
+        public static List<MyAction> GetTestDataList(MyModule module)
+        {
+            var resultList = new List<MyAction>();
+
+            resultList.Add(new MyAction()
+            {
+                ModuleCode = module.ModuleCode,
+                ActionCode = $"{module.ModuleCode}_I",
+                ActionName = $"新增 {module.ModuleName}",
+            });
+
+            resultList.Add(new MyAction()
+            {
+                ModuleCode = module.ModuleCode,
+                ActionCode = $"{module.ModuleCode}_U",
+                ActionName = $"编辑 {module.ModuleName}",
+            });
+
+            resultList.Add(new MyAction()
+            {
+                ModuleCode = module.ModuleCode,
+                ActionCode = $"{module.ModuleCode}_D",
+                ActionName = $"删除 {module.ModuleName}",
+            });
+
+            return resultList;
+        }
+
+    }
+}
---
+++ W4101_AntDesignApp/W4113_AntDesignProServer/Models/Test/MyModule.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace W4113_AntDesignProServer.Models.Test
+{
+
+
+    /// <summary>
+    /// 功能模块.
+    /// </summary>
+    [DisplayName("功能模块")]
+    public class MyModule
+    {
+
+
+        /// <summary>
+        /// 模块编号.
+        /// </summary>
+        [Display(Name = "模块编号")]
+        [Required]
+        public string ModuleCode { set; get; }
+
+
+
+        /// <summary>
+        /// 模块名称.
+        /// </summary>
+        [Display(Name = "模块名称")]
+        [Required]
+        public string ModuleName { set; get; }
+
+
+
+
+        /// <summary>
+        /// 模块下的 动作列表.
+        /// </summary>
+        public List<MyAction> Actions { set; get; }
+
+
+
+
+
+
+        public static List<MyModule> GetTestDataList(int dataCount = 10)
+        {
+            var resultList = new List<MyModule>();
+
+            for (int i = 0; i < dataCount; i++)
+            {
+                var oneResult = new MyModule()
+                {
+                    ModuleCode = $"TEST_{i + 1:000}",
+                    ModuleName = $"測試模組_{i + 1:000}",
+                };
+
+                resultList.Add(oneResult);
+            }
+            return resultList;
+        }
+
+
+
+    }
+}
